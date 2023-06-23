cc5d7d2fe7825bbe3f0107d7e92af8bfc0f4199a
Giresharu <gireshiyaru@gmail.com>
2023-06-23 23:42:58 +0800

    去除TMProPlayer多余的using

--- Assets/Runtime/TMProPlayer.cs
+++ Assets/Runtime/TMProPlayer.cs
@@ -1,8 +1,5 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Text;
-using System.Text.RegularExpressions;
 using System.Threading;
 using TMPro;
 using UnityEngine;
